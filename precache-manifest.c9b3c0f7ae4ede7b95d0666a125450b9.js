f13c1c1f101b33c36dd18de2e31e00be872e603d
UttamUk <33575994+UttamUk@users.noreply.github.com>
2020-09-05 05:49:17 +0530

    Add files via upload

---
+++ precache-manifest.c9b3c0f7ae4ede7b95d0666a125450b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f2e19b246cb42898643ae3a343a643",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "2b0ddb97817844ca1b58",
+    "url": "/Test/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "4f53b96f487c474a3b8b",
+    "url": "/Test/static/js/2.a85e94d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test/static/js/2.a85e94d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0ddb97817844ca1b58",
+    "url": "/Test/static/js/main.a2482fd9.chunk.js"
+  },
+  {
+    "revision": "98a821c223076286c47b",
+    "url": "/Test/static/js/runtime-main.1c4d33b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
